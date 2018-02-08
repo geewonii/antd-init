d07e3b381b9d717a3caee9aac4f22233cce2319d
geewonii <598442441@qq.com>
2018-02-08 16:50:20 +0800

    请相信，我已经崩溃了

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+require('isomorphic-fetch');
 import Login from 'ant-design-pro/lib/Login';
-import { Alert, Checkbox } from 'antd';
-import './newlogin180208.css';
+import { Alert, Checkbox, message } from 'antd';
+import './index.css';
 
 const { Tab, UserName, Password, Mobile, Captcha, Submit } = Login;
 
@@ -11,29 +12,50 @@ class App extends React.PureComponent {
     notice: '',
     type: 'tab2',
     autoLogin: true,
+    captcha: false,
+    mobile: '',
   }
+  componentDidMount() {
+    const {autoLogin} = this.state
+    if(autoLogin) {
+      const Mobiles = localStorage.getItem("phoneleeMobile")
+    }
+    fetch('./package.json')
+    .then(response => {
+        if (response.status >= 400) {
+            message.error("服务器未响应")
+            throw new Error("Bad response from server")
+        }
+        return response.json();
+    })
+    .then(stories => {
+        console.log(stories);
+    })
+  }
+
+  onTabChange = key => this.setState({type: key})
+
+  updateMobile = e => this.setState({mobile: e.target.value})
+
+  changeAutoLogin = e => this.setState({autoLogin: e.target.checked})
+
+  getCaptcha = () => {
+    if(this.state.mobile.match(/^1\d{10}$/)){
+      this.setState({captcha: true})
+    }
+  }
+
   onSubmit = (err, values) => {
     console.log('value collected ->', { ...values, autoLogin: this.state.autoLogin });
     if (this.state.type === 'tab1') {
-      this.setState({
-        notice: '',
-      }, () => {
-        if (!err && (values.username !== 'admin' || values.password !== '888888')) {
-          setTimeout(() => {
-            this.setState({
-              notice: '账号或密码错误！',
-            });
-          }, 500);
+      this.setState({notice: ''}, () => {
+        if (!err && (values.mobile !== '13632422291' || values.PassWord !== '1')) {
+          this.setState({notice: '账号或密码错误！'});
         }
       });
     }
   }
-  onTabChange = (key) => {
-    this.setState({type: key,});
-  }
-  changeAutoLogin = (e) => {
-    this.setState({autoLogin: e.target.checked,});
-  }
+
   render() {
     return (
       <Login
@@ -46,24 +68,28 @@ class App extends React.PureComponent {
             this.state.notice &&
             <Alert style={{ marginBottom: 24 }} message={this.state.notice} type="error" showIcon closable />
           }
-          <UserName name="username" />
-          <Password name="password" />
+          <Mobile name="mobile" onChange={this.updateMobile} />
+          <Password name="PassWord" />
         </Tab>
         <Tab key="tab2" tab="手机登录">
-          <Mobile name="mobile" />
-          <Captcha onGetCaptcha={() => console.log('Get captcha!')} name="captcha" />
+          <Mobile name="Mobile" onChange={this.updateMobile} />
+          <Captcha onGetCaptcha={ this.getCaptcha } name="captcha" />
+          {
+            this.state.captcha &&
+            <Alert style={{ marginBottom: 24 }} message="已发送短信验证码，请注意查收" type="info" showIcon />
+          }
         </Tab>
         <div>
           <Checkbox checked={this.state.autoLogin} onChange={this.changeAutoLogin}>自动登录</Checkbox>
-          <a style={{ float: 'right' }} href="">忘记密码</a>
+          <a style={{ float: 'right' }} href="https://www.phonelee.com/God/ForgotPassword">忘记密码</a>
         </div>
         <Submit>登录</Submit>
         <div>
-          <a style={{ float: 'right' }} href="">注册账户</a>
+          <a style={{ float: 'right' }} href="https://www.phonelee.com/God/Create">注册账户</a>
         </div>
       </Login>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('newLogin'));
